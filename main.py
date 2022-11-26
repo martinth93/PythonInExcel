849b9d2c61014e251f47bc6643002b00940c63f4
martinth93 <martin.thelen96@googlemail.com>
2022-11-26 21:48:09 +0100

    1

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 import datetime
+import time
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (
+                               FormatStrFormatter,
+                               AutoMinorLocator,
+                               FuncFormatter,
+                              )
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
 
 
 # def main():
@@ -103,6 +112,83 @@ import datetime
 #     df['Last Name'] = df['Name'].str.split(',', expand=True)[0]
 #     print(df)
 
+
+# def main():
+#     def z(x, y):
+#         return x**2 + y**(1/3)
+#
+#     def goal_seek(target, _threshold):
+#         threshold = _threshold
+#         lower = 0
+#         upper = 1000000
+#         solve = (lower + upper)/2
+#         while abs(threshold) >= _threshold:
+#             print(f'Threshold is: {threshold}')
+#             print(f'range is: {lower} ---- {solve} ---- {upper}')
+#             if threshold < 0:
+#                 upper = solve
+#                 solve = (lower + upper)/2
+#             elif threshold > 0:
+#                 lower = solve
+#                 solve = (lower + upper)/2
+#             threshold = target - z(3, solve)
+#
+#         print(f'Final result: Threshold: {threshold}....Solved input: {solve}')
+#         return solve
+#
+#     start = time.time()
+#     goal_seek(90, 0.00001)
+#
+#     print(f'Finished in {time.time()-start} seconds')
+
+#
+# def main():
+#     list_1 = [1, 2, 3, 4, 5, 6, 7]
+#     list_2 = ["a", "b", "c", "d", "e", "f", "g"]
+#     df = pd.DataFrame(zip(list_1, list_2))
+#     print(df)
+
+# def main():
+#     df = pd.read_excel('find_replace.xlsx')
+#     # print(df.replace('Ayanami Rei', 'Yui Ikari'))
+#     df.loc[df['Pilot'] == 'Kaworu Nagisa', 'Side'] = 'Enemy'
+#     print(df)
+
+def main():
+    df = pd.read_csv('https://raw.githubusercontent.com'
+                     '/CSSEGISandData/COVID-19/master/'
+                     'csse_covid_19_data/csse_covid_19_time_series/'
+                     'time_series_covid19_confirmed_global.csv')
+    df = df.iloc[:, 4:]
+    global_num = df.sum()
+
+    fig, ax = plt.subplots()
+    ax.set(title='Global Covid Confirmed Cases',
+           xlabel='Time',
+           ylabel='Number of cases')
+
+    ax.yaxis.set_major_formatter(FuncFormatter(
+        lambda x, p:format(x/1000000) + 'M'))
+
+    date_form = DateFormatter("%d-%m-%y")
+    ax.xaxis.set_major_formatter(date_form)
+    locator = mdates.DayLocator()
+    ax.xaxis.set_major_locator(locator)
+
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=12))
+    ax.yaxis.grid()
+
+    dates = pd.to_datetime(global_num.index)
+    plt.xticks(rotation=45)
+    plt.plot(dates, global_num)
+    # plt.show()
+
+    plt.savefig('python_pretty_plot.png')
+    writer = pd.ExcelWriter('python_plot.xlsx')
+    global_num.to_excel(writer, sheet_name='Sheet1', engine='xlsxwriter')
+    worksheet = writer.sheets['Sheet1']
+    worksheet.insert_image('C2', 'python_pretty_plot.png')
+    writer.save()
 
 if __name__ == "__main__":
     main()
